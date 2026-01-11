67e4b7a6d9e306676eab513f8a5bb70cc8370484
Mohamed Saleh <smaratrader2005@gmail.com>
2026-01-12 00:10:55 +0200

    Footer11

--- Components/Footer&Contact/Footer.tsx
+++ Components/Footer&Contact/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { FaAngleDoubleUp } from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className=' flex justify-between px-7 items-center flex-col md:flex-row'>
+        <p className='text-sm order-2 md:order-1'>© 2026 Mohamed Saleh. All rights reserved.</p>
+        <a href='#' className='My_Hover mb-3 order-1 md:order-2'><FaAngleDoubleUp /></a>
+    </div>
   )
 }
--- Components/Header/Navbar.tsx
+++ Components/Header/Navbar.tsx
@@ -5,7 +5,7 @@ import Phonenav from './Phonenav'
 
 export default function Navbar() {
     return (
-        <header className='Special w-full h-20 px-8 flex items-center justify-between shadow-md z-10 fixed'>
+        <header className='SpecialNav w-full h-20 px-8 flex items-center justify-between shadow-md z-10 fixed'>
 
             <a href='/' className='myfont font-semibold text-sky-400 text-xl '>  M <span className='h1 text-black'>0</span>  </a>
             <NavItems />
